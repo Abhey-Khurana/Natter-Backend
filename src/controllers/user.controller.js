722f8f68b40abe81bf34f35998d058a82b5d17b2
Abhey Khurana <abheykhurana73@gmail.com>
2024-05-23 00:46:23 +0530

    Set Avatar functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,7 @@ async function register(req, res) {
     try {
         const { username, email, password } = req.body;
         const userNameCheck = await User.findOne({ username });
-        console.log(userNameCheck);
+        // console.log(userNameCheck);
         if (userNameCheck) {
             return res.json({ message: "Username already exists.", status: false });
         }
@@ -25,10 +25,11 @@ async function register(req, res) {
         });
 
         delete user.password;
+        console.log(user);
         return res.status(201).json({ message: "User created Successfully.", status: true, user });
     }
     catch (err) {
-        console.log(err);
+        next(err);
     }
 
 
@@ -58,8 +59,33 @@ async function login(req, res) {
         return res.status(201).json({ message: "User Logged in Successfully.", status: true, user });
     }
     catch (err) {
-        console.log(err);
+        next(err);
     }
 }
 
-export { register, login };
+async function setAvatar(req, res, next) {
+    console.log(req.params);
+    try {
+        const userId = req.params.id;
+        const avatarImage = req.body.avatarImage;
+
+        const userData = await User.findByIdAndUpdate(
+            userId,
+            {
+              isAvatarImageSet: true,
+              avatarImage,
+            },
+            { new: true }
+          );
+
+        return  res.status(200).json({
+                isSet: userData.isAvatarImageSet,
+                image: userData.avatarImage
+        });
+    }
+    catch (err) {
+        next(err);
+    }
+
+}
+export { register, login, setAvatar };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { register ,login} from "../controllers/user.controller.js";
+import { register ,login,setAvatar} from "../controllers/user.controller.js";
 
 const router=Router();
 
 
 router.post("/register",register)
 router.post("/login",login);
-
+router.post("/setavatar/:id",setAvatar);
 
 export default router
